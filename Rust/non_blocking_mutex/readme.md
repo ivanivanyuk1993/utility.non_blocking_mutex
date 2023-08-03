d0f62fb7cb9ab31cae39fcc1d0f3716ee17ad9ec
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2023-08-04 02:40:22 +0300

    Added examples in comments and `readme.md`

--- Rust/non_blocking_mutex/readme.md
+++ Rust/non_blocking_mutex/readme.md
@@ -10,3 +10,16 @@ when contention is high enough
 
 Notice that `NonBlockingMutex` doesn't guarantee order
 of execution, only atomicity of operations is guaranteed
+
+# Examples
+```rust
+use non_blocking_mutex::NonBlockingMutex;
+use std::thread::{available_parallelism};
+
+let max_concurrent_thread_count = available_parallelism().unwrap().get();
+
+let non_blocking_mutex = NonBlockingMutex::new(max_concurrent_thread_count, 0);
+non_blocking_mutex.run_if_first_or_schedule_on_first(|mut state| {
+    *state += 1;
+});
+```
--- Rust/non_blocking_mutex/src/lib.rs
+++ Rust/non_blocking_mutex/src/lib.rs
@@ -21,6 +21,19 @@ pub struct NonBlockingMutex<'captured_variables, State: ?Sized> {
 /// [NonBlockingMutex] doesn't guarantee order of execution too, even of already added
 /// items
 ///
+/// # Examples
+/// ```rust
+/// use non_blocking_mutex::NonBlockingMutex;
+/// use std::thread::{available_parallelism};
+///
+/// let max_concurrent_thread_count = available_parallelism().unwrap().get();
+///
+/// let non_blocking_mutex = NonBlockingMutex::new(max_concurrent_thread_count, 0);
+/// non_blocking_mutex.run_if_first_or_schedule_on_first(|mut state| {
+///     *state += 1;
+/// });
+/// ```
+///
 /// ```
 /// use non_blocking_mutex::NonBlockingMutex;
 ///
